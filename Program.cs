a151767b91b76a7574f5e3dfdf2cf622c4ff77a4
Enzo <113740825+oliveiraenzo@users.noreply.github.com>
2022-10-07 16:09:35 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Projeto_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 1");
+
+            //Variáveis
+            string nome, endereço, telefone;
+
+            //Entrada de dados
+            Console.WriteLine("Digite o Nome do Aluno:");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o endereço:");
+            endereço = Console.ReadLine();
+
+            Console.WriteLine("Digite o telefone:");
+            telefone = Console.ReadLine();
+
+            //Saída de dados
+            Console.WriteLine($"Olá {nome}, seu endereço é {endereço} e seu telefone é {telefone}.");
+
+            Console.ReadKey();
+        }
+    }
+}
